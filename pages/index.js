9c4ac0e10a807721de907263972ed098d465bff8
joyjsmun <joy.js.mun@gmail.com>
2022-02-23 12:07:58 -0800

    ADD: movie detail

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,76 @@
-import { useEffect,useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
-
-export default function Home({results}){
-  
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
-      {results?.map( (movie) => (
-      <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>))}
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
+        </div>
+      ))}
       <style jsx>{`
-        .container{
-          display:grid;
+        .container {
+          display: grid;
           grid-template-columns: 1fr 1fr;
-          padding:20px;
-          gap:20px;
+          padding: 20px;
+          gap: 20px;
         }
-        .movie{
-          cursor:pointer;
+        .movie {
+          cursor: pointer;
         }
-
-        .movie img{
-          max-width:100%;
-          border-radius:12px;
-          transition:transform 0.2s ease-in-out;
-          box-shadow:rgba(0,0,0,0.1) 0px 4px 12px;
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
-
-        .movie:hover img{
+        .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
-
-        .movie h4{
-          font-size:18px;
-          text-align:center;
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
         }
-        
-        `}
-      </style>
+      `}</style>
     </div>
   );
 }
 
-
 export async function getServerSideProps() {
   const { results } = await (
-    await fetch(`http://localhost:3000/api/movies`)
+    await fetch(`http://localhost:3001/api/movies`)
   ).json();
   return {
     props: {
